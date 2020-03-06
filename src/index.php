f1f54aaa56e23a970d494d99220b21616327cdb3
Pavlo Saikevych <Pavlo.Saikevych@west.com>
2020-03-06 13:46:05 +0200

    source files changed

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World from Psaik! <br>";
+echo "Hello, World from Psaik! New version was deployed from GitHub actions!<br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
