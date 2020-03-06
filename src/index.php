b4c60fa1a187d1d2e92009841be79d04861d02b7
Pavlo Saikevych <Pavlo.Saikevych@west.com>
2020-03-06 19:34:20 +0200

    weekly report

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "Hello, World from Psaik! New version was deployed from GitHub actions!!<br>";
-echo "Hello ECS! <br>";
+echo "Hello Weekly Report! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
 ?>
